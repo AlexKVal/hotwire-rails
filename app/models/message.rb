2be8baee456d818c323d0b0b45387b80e2574d36
Alexander Shemetovskiy <alexkval@gmail.com>
2021-04-05 00:33:47 +0100

    Use broadcasts_to to enable all the CRUD operations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,9 @@
 class Message < ApplicationRecord
   belongs_to :room
-  after_create_commit -> { broadcast_append_to room }
+
+  broadcasts_to :room # one line instead of the three below
+  # after_create_commit -> { broadcast_append_to room }
+  # after_destroy_commit -> { broadcast_remove_to room }
+  # after_update_commit -> { broadcast_replace_to room }
+
 end
